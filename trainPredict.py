ea00e32d8b12ec1c79d1a9097ac59f1c5d7c65dd
Jessica Choi <choi.jess816@gmail.com>
2018-03-09 01:13:33 -0800

    Create naive sonnet with 14 lines and 10 syllables each

--- trainPredict.py
+++ trainPredict.py
@@ -4,7 +4,7 @@ from preprocessing import read_files, featurize, block_text, read_text
 import numpy as np
 
 
-def generate_words(emission, POSlookup):
+def generate_words(emission, POSlookup, syllables):
 	'''
 	This function generates a string given the emissions and the probabilities 
 	of a word being emitted given a certain
@@ -17,13 +17,22 @@ def generate_words(emission, POSlookup):
 		emStr: The sentence generated
 	'''
 	emStr = ''
-	for obs in emission: 
-		emRate = [row[1] for row in POSlookup[obs]]
-		emWords = [row[0] for row in POSlookup[obs]]
-		emRate = np.array(emRate)
-		emRate = emRate/sum(emRate)
-		index = np.random.choice(np.arange(len(emRate)), p=emRate)
-		emStr = emStr + emWords[index] + ' '
+	syllableCount = 0
+	done = False
+	while not done:
+		for obs in emission: 
+			emRate = [row[1] for row in POSlookup[obs]]
+			emWords = [row[0] for row in POSlookup[obs]]
+			emRate = np.array(emRate)
+			emRate = emRate/sum(emRate)
+
+			index = np.random.choice(np.arange(len(emRate)), p=emRate)
+			new_word = emWords[index]
+			syllableCount += syllables[new_word]
+			emStr = emStr + new_word + ' '
+			if syllableCount == 10:
+				done = True
+
 	return emStr
 
 
@@ -35,7 +44,9 @@ def generate_words(emission, POSlookup):
 # print(generate_words(beeEm, beeLookup))
 
 
-poems, syllables = read_files(sep='poem')
+poems, syllables, _ = read_files(sep='poem')
+lines, syllables, rhymes = read_files(sep='line')
+# print (rhymes)
 POSList, POSlookup, features = featurize(poems)
 
 # this takes ~10 minutes to run
@@ -44,8 +55,13 @@ HMM = unsupervised_HMM(features, 10, 100)
 # emission is much less than 30
 
 
-emission, states = HMM.generate_emission(20)
-print(generate_words(emission, POSlookup))
+sonnet = ""
+for i in range(14):
+	emission, states = HMM.generate_emission(10)
+	line = generate_words(emission, POSlookup, syllables)
+	sonnet = sonnet + line + "\n"
+
+print(sonnet)
 
 # wow our sentence is fucking retarded. 
 # golden it of your which of gain term with whereon without her respect when you praising the face which to 
